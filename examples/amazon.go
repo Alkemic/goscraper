2cdaa8e7e5b4b37d5e6fa5b5205c2ef89e910870
Daniel Alkemic Czuba <alkemic7@gmail.com>
2018-07-28 10:05:12 +0200

    Better error handling & small refactor

--- examples/amazon.go
+++ examples/amazon.go
@@ -5,57 +5,62 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"strings"
 
 	"github.com/Alkemic/goscraper"
 	"github.com/moovweb/gokogiri/xml"
 )
 
 func main() {
+	processPrice := func(n *xml.Node) (interface{}, error) {
+		f, err := strconv.ParseFloat((*n).Content()[2:], 64)
+		if err != nil {
+			return nil, err
+		}
+		return f, nil
+	}
+	processImage := func(n *xml.Node) (interface{}, error) {
+		var f map[string]interface{}
+		// this part quite naive, and there should be added some
+		// validation, but hey! it's only an example ;-)
+		attr := (*n).Attribute("data-a-dynamic-image").String()
+		err := json.Unmarshal([]byte(attr), &f)
+		if err != nil {
+			return nil, err
+		}
+
+		for k := range f {
+			return k, nil
+		}
+
+		return nil, nil
+	}
+	processTitle := func(n *xml.Node) (interface{}, error) {
+		return strings.TrimSpace((*n).Content()), nil
+	}
+
 	scrape := &goscraper.Scrape{
-		Fields: map[string]*goscraper.Field{
-			"title": &goscraper.Field{Selector: "//*[@id=\"productTitle\"]"},
-			"price": &goscraper.Field{
-				Selector: "//*[@id=\"unqualifiedBuyBox\"]/div/div[1]/span",
-				Callback: func(n *xml.Node) interface{} {
-					f, err := strconv.ParseFloat((*n).Content()[2:], 64)
-					if err != nil {
-						log.Println(err)
-						return nil
-					}
-					return f
-				},
-			},
-			"image": &goscraper.Field{
-				Selector: "//*[@id=\"landingImage\"]",
-				Callback: func(n *xml.Node) interface{} {
-					var f map[string]interface{}
-					// this part quite naive, and there should be added some
-					// validation, but hey! it's only an example ;-)
-					attr := (*n).Attribute("data-a-dynamic-image").String()
-					err := json.Unmarshal([]byte(attr), &f)
-					if err != nil {
-						log.Println(err)
-						return nil
-					}
-
-					for k := range f {
-						return k
-					}
-
-					return nil
-				},
-			},
+		Fields: map[string]goscraper.Field{
+			"title": {Selector: "//*[@id=\"productTitle\"]", Callback: processTitle},
+			"price": {Selector: "//*[@id=\"unqualifiedBuyBox\"]/div/div[1]/span", Callback: processPrice},
+			"image": {Selector: "//*[@id=\"landingImage\"]", Callback: processImage},
 		},
 		Headers: map[string]string{
-			"accept-encoding": "gzip, deflate, sdch",
-			"cache-control":   "no-cache",
-			"accept-language": "pl-PL,pl;q=0.8,en-US;q=0.6,en;q=0.4",
+			"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+			"accept-language":           "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+			"cache-control":             "no-cache",
+			"cookie":                    "session-id=259-7520693-7366814; session-id-time=2082787201l; ubid-acbuk=259-2899942-1087152",
+			"dnt":                       "1",
+			"pragma":                    "no-cache",
+			"upgrade-insecure-requests": "1",
+			"user-agent":                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36",
 		},
 	}
 
-	fmt.Println(scrape.ProcessURL(
-		"http://www.amazon.co.uk/Lenovo-ThinkPad-T430s-i5-3320M-N1RLQGE/" +
-			"dp/B00AQPBA8U/ref=sr_1_1?ie=UTF8&qid=1455007797&" +
-			"sr=8-1&keywords=t430s",
-	))
+	url := "http://www.amazon.co.uk/Lenovo-ThinkPad-T430s-i5-3320M-N1RLQGE/dp/B00AQPBA8U/ref=sr_1_1?ie=UTF8&qid=1455007797&sr=8-1&keywords=t430s"
+	scraped, err := scrape.ProcessURL(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(scraped)
 }
--- scrape.go
+++ scrape.go
@@ -8,70 +8,94 @@ import (
 	"github.com/moovweb/gokogiri/html"
 	"github.com/moovweb/gokogiri/xml"
 	"github.com/moovweb/gokogiri/xpath"
+	"github.com/pkg/errors"
 )
 
-// Type Field defines how data should be selected and process
+// Field defines how data should be selected and process
 type Field struct {
 	// XPath selector
 	Selector string
 	// Callback is used to post process selected field
-	Callback func(*xml.Node) interface{}
+	Callback func(n *xml.Node) (interface{}, error)
 }
 
-// ProcessField method fetches data from passed document
-func (f *Field) ProcessField(d *html.HtmlDocument) interface{} {
-	var value interface{}
-	var node xml.Node
+// processField method fetches data from passed document
+func (f Field) processField(d *html.HtmlDocument) (interface{}, error) {
 	selector := xpath.Compile(f.Selector)
-	result, _ := d.Root().Search(selector)
+	result, err := d.Root().Search(selector)
+	if err != nil {
+		return nil, err
+	}
 
+	var node xml.Node
 	if len(result) > 0 {
 		node = result[0]
 	} else {
-		return ""
+		return "", nil
 	}
 
+	var value interface{}
 	if f.Callback != nil {
-		value = f.Callback(&node)
+		value, err = f.Callback(&node)
+		if err != nil {
+			return nil, errors.Wrap(err, "error processing callback")
+		}
 	} else {
 		value = node.Content()
 	}
 
-	return value
+	return value, nil
 }
 
 // Scrape type defines what to scrape and headers used in HTTP request, and
 // allows to process data
 type Scrape struct {
 	// definitions of fields that will be scraped
-	Fields map[string]*Field
+	Fields map[string]Field
 	// HTTP headers used in query
 	Headers map[string]string
 }
 
 // ProcessURL method is used to fetch website contents and process it's data
-func (s *Scrape) ProcessURL(url string) map[string]interface{} {
+func (s *Scrape) ProcessURL(url string) (map[string]interface{}, error) {
 	client := &http.Client{}
 
-	req, _ := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't create new request")
+	}
 	for k, v := range s.Headers {
 		req.Header.Add(k, v)
 	}
-
-	resp, _ := client.Do(req)
-	page, _ := ioutil.ReadAll(resp.Body)
-	doc, _ := gokogiri.ParseHtml(page)
-	defer doc.Free()
-
-	return s.ProcessDocument(doc)
-}
-
-// ProcessDocument is used to process document fetched from website
-func (s *Scrape) ProcessDocument(d *html.HtmlDocument) map[string]interface{} {
-	data := make(map[string]interface{})
-	for key := range s.Fields {
-		data[key] = s.Fields[key].ProcessField(d)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't create new request")
 	}
 
-	return data
+	page, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't read response body")
+	}
+
+	doc, err := gokogiri.ParseHtml(page)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't parse html")
+	}
+	defer doc.Free()
+
+	return s.processDocument(doc)
+}
+
+// processDocument is used to process document fetched from website
+func (s *Scrape) processDocument(d *html.HtmlDocument) (map[string]interface{}, error) {
+	data := make(map[string]interface{})
+	var err error
+	for key := range s.Fields {
+		data[key], err = s.Fields[key].processField(d)
+		if err != nil {
+			return nil, errors.Wrapf(err, "can't process field %s", key)
+		}
+	}
+
+	return data, nil
 }
